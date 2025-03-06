00f00560c31d335522e4bf9561765de23f9929cd
Veldrine Evelia Kaharwa <eveliaveldrine@outlook.com>
2025-03-06 23:23:29 +0300

    Update web-push.ts

--- src/lib/web-push.ts
+++ src/lib/web-push.ts
@@ -1,14 +1,17 @@
-import webPush from "web-push"
+import webPush from "web-push";
 
-const publicVapidKey = process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY
-const privateVapidKey = process.env.VAPID_PRIVATE_KEY
-const webPushContact = process.env.NEXT_PUBLIC_SITE_URL
+const publicVapidKey = process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY;
+const privateVapidKey = process.env.VAPID_PRIVATE_KEY;
+
+// Use a mailto: URL instead of http://localhost
+const webPushContact = process.env.VAPID_CONTACT_EMAIL 
+  ? `mailto:${process.env.VAPID_CONTACT_EMAIL}` 
+  : "mailto:TheDevJournal@protonmail.com";
 
 if (!publicVapidKey || !privateVapidKey) {
-    console.warn("Web Push requires VAPID keys to be set")
+    console.warn("Web Push requires VAPID keys to be set");
 }
 
-webPush.setVapidDetails(webPushContact || "https://localhost:3000", publicVapidKey || "", privateVapidKey || "")
+webPush.setVapidDetails(webPushContact, publicVapidKey || "", privateVapidKey || "");
 
-export { webPush }
-
+export { webPush };
